94e62edd0bcd76a598d77d3f45419093847cfe8e
Manish Kumar <66223643+manishexperiau@users.noreply.github.com>
2022-08-08 00:46:24 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -162,3 +162,22 @@ $(document).ready(function(){
         }
     });
 });
+
+
+
+setInterval(setClock, 1000);
+const hrHand = document.getElementById('hour'),
+ minHand = document.getElementById('minute'),
+ secHand = document.getElementById('second');
+ 
+function setClock(){
+    const currentDate = new Date()
+    const sec = currentDate.getSeconds() * 6;
+    const min = currentDate.getMinutes() * 6;
+    const hr = currentDate.getHours() * 30;
+
+    secHand.style.transform = `rotate(${sec}deg)`;
+    minHand.style.transform = `rotate(${min}deg)`;
+    hrHand.style.transform = `rotate(${(hr) + (min / 12)}deg)`;
+}
+setClock();
